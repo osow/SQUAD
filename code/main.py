fe595b3085ac86af6a86334089f0cfe4c03fb444
Omar William Sow <osow@rice15.stanford.edu>
2018-03-12 21:25:57 -0700

    caught a couple of things

--- code/main.py
+++ code/main.py
@@ -128,9 +128,9 @@ def main(unused_argv):
 
     #IF IN EARLY TRAINING:
 
-    train_context_path = os.path.join(FLAGS.data_dir, "mini-train.context")
-    train_qn_path = os.path.join(FLAGS.data_dir, "mini-train.question")
-    train_ans_path = os.path.join(FLAGS.data_dir, "mini-train.span")
+    train_context_path = os.path.join(FLAGS.data_dir, "train.context")
+    train_qn_path = os.path.join(FLAGS.data_dir, "train.question")
+    train_ans_path = os.path.join(FLAGS.data_dir, "train.span")
 
     #ELSE, USE FULL DATA:
     '''train_context_path = os.path.join(FLAGS.data_dir, "train.context")
